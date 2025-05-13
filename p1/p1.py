053274d333d07f34529681aa4fc49b67b6268b1a
Vignesh Vanchinathan <viggy.vanchi@gmail.com>
2025-05-13 13:35:22 -0500

    fixed. LESS THAN

--- p1/p1.py
+++ p1/p1.py
@@ -2,11 +2,11 @@
 # instead do a lil rearranging
 
 def multiplesUpto(n, M): 
-    return M//n #get number of multiples of n upto M 
+    return (M-1)//n #get number of multiples of n upto M 
 def sumN(n): 
     return (n+1) * (n/2) # sum of natural nums upto n
 
 #don't double count
 print(3 * sumN(multiplesUpto(3,1000)) + 5 * sumN(multiplesUpto(5, 1000)) - 15 * sumN(multiplesUpto(15, 1000)))
 
-# 234168
+# 233168
